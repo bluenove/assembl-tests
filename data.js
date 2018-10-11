2fb0dbef758f7be71801e947b0094a0975e2b6ee
Felix Saint-Genis <saintgenisfelix@gmail.com>
2018-10-11 17:47:32 +0200

    add debug function

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 const data = {
-  url: "https://dev-assembl.bluenove.com/felixdebate"
+  url: 'https://dev-assembl.bluenove.com/felixdebate'
 };
 
 module.exports = data;
--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,5 +1,12 @@
+const isDebugging = () => {
+  let launch = {
+    headless: false,
+    slowMo: 50,
+    devtools: true
+  };
+  return process.env.NODE_ENV === 'debug' ? launch : {};
+};
+
 module.exports = {
-  launch: {
-    headless: true
-  }
+  launch: isDebugging()
 };
