c441f3186a97f5a5762caebf7505f927043b5479
Milan <milan.barande@hotmail.fr>
2018-10-15 17:19:24 +0200

    refactor signIn and add 2nd user data

--- data.js
+++ data.js
@@ -1,8 +1,12 @@
 const data = {
-  url: "http://localhost:6543/felixdebate",
-  user: {
-    email: "assemble2etesting@gmail.com",
-    password: "123456"
+  url: 'http://localhost:6543/felixdebate',
+  user1: {
+    email: 'assemble2etesting@gmail.com',
+    password: '123456'
+  },
+  user2: {
+    email: 'oui@oui.com',
+    password: 'salut'
   }
 };
 
--- tests/signin.test.js
+++ tests/signin.test.js
@@ -1,41 +1,34 @@
-const data = require("../data");
-
-const signIn = async () => {
-  await page.goto(`${data.url}/login`);
-  await page.waitForSelector(".login-container");
-  await page.click("input[name=identifier]");
-  await page.type("input[name=identifier]", data.user.email);
-  await page.click("input[name=password]");
-  await page.type("input[name=password]", data.user.password);
+const data = require('../data');
+const { url, user1 } = data;
+const signIn = async (
+  baseUrl = url,
+  email = user1.email,
+  password = user1.password
+) => {
+  await page.goto(`${baseUrl}/login`);
+  await page.waitForSelector('.login-container');
+  await page.click('input[name=identifier]');
+  await page.type('input[name=identifier]', email);
+  await page.click('input[name=password]');
+  await page.type('input[name=password]', password);
   await page.click("button[name='login']");
   // check if the profile menu is visible
-  await page.waitForSelector(".user-account");
-  const profileMenu = await page.$eval(
-    "#user-dropdown",
-    el => (el ? true : false)
-  );
+  await page.waitForSelector('.user-account');
 };
 
-describe("Assembl signin", () => {
+describe('Assembl signin', () => {
   beforeEach(async () => {
     jest.setTimeout(100000);
-    await page.goto(`${data.url}/login`);
+    await page.goto(`${url}/login`);
   });
 
-  test("I can sign in assembl", async () => {
-    await page.waitForSelector(".login-container");
-    await page.click("input[name=identifier]");
-    await page.type("input[name=identifier]", data.user.email);
-    await page.click("input[name=password]");
-    await page.type("input[name=password]", data.user.password);
-    await page.click("button[name='login']");
-    // check if the profile menu is visible
-    await page.waitForSelector(".user-account");
+  test('I can sign in assembl', async () => {
+    await signIn();
     const profileMenu = await page.$eval(
-      "#user-dropdown",
+      '#user-dropdown',
       el => (el ? true : false)
     );
-    expect(profileMenu).toBe(true);
+    await expect(profileMenu).toBe(true);
   });
 });
 
