407f4f23e0163912a6b6fafb9342b0cdee93e572
Milan <milan.barande@hotmail.fr>
2018-10-23 10:34:47 +0200

    add --disabled-dev-shm-usage as browser argument

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -4,10 +4,12 @@ const isDebugging = () => {
     devtools: false,
     timeout: 0,
     slowMo: 20,
-    args: ["--start-fullscreen"],
+    args: ['--start-fullscreen', '--disable-dev-shm-usage'],
     defaultViewport: null
   };
-  return process.env.NODE_ENV === "debug" ? launch : {};
+  return process.env.NODE_ENV === 'debug'
+    ? launch
+    : { args: ['--disabled-dev-shm-usage'] };
 };
 
 module.exports = {
